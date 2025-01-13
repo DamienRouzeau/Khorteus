17b502bcbf8d3395e86fde0fbcd7652a42f1f319
Damien <86470640+DamienRouzeau@users.noreply.github.com>
2025-01-13 17:04:05 +0100

    fragment spawn

---
+++ Assets/Scripts/Fragments/FragmentType.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FragmentType", menuName = "ScriptableObject/FragmentType", order = 3)]
+public class FragmentType : ScriptableObject
+{
+    public float maxHealth;
+    public float health;
+    public float probability;
+    public float minTime;
+    public float maxTime;
+    public GameObject visual;
+    public FragmentType nextEvolution;
+}
